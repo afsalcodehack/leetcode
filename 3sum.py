00a5cd7ee5e0142b72651179de9f74ae83b809b7
mycodehack@gmail.com <afsal.aimit.2010@gmail.com>
2019-11-15 14:18:25 +0300

    3sum

--- 3sum.py
+++ 3sum.py
@@ -1,14 +1,14 @@
 def comb(arr, l, p, temp):
+
     if l == len(temp):
         print(temp)
         return
 
     for i in range(p, len(arr)):
         temp.append(arr[i])
-        comb(arr, l, p + 1, temp)
+        comb(arr, l, i + 1, temp)
         del temp[-1]
 
-
-val = [-1, 0, 1, 2, -1, -4]
+val = [-1, 0, 1, 2,3,4]
 t = [] * 3
 comb(val, 3, 0, t)
