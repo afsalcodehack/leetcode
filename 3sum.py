10ea821c1e59bd6d09677daf317428ffb1309801
afsal <mycodehack@gmail.com>
2019-11-15 15:01:57 +0300

    three sum change

--- 3sum.py
+++ 3sum.py
@@ -1,14 +1,24 @@
-def comb(arr, l, p, temp):
-    if l == len(temp):
-        print(temp)
-        return
+class threesum:
+    result = []
+    def comb(self ,arr, k, p, temp):
+        if k == len(temp):
+            if (temp[0] + temp[1] + temp[2]) == 0:
+                temp.sort()
+                if temp not in self.result:
+                    self.result.append(temp[:])
+            return
 
-    for i in range(p, len(arr)):
-        temp.append(arr[i])
-        comb(arr, l, p + 1, temp)
-        del temp[-1]
+        for i in range(p, len(arr)):
+            temp.append(arr[i])
+            self.comb(arr, k, i + 1, temp)
+            del temp[-1]
+
 
 
 val = [-1, 0, 1, 2, -1, -4]
 t = [] * 3
-comb(val, 3, 0, t)
+service = threesum()
+service.comb(val, 3, 0, t)
+
+print(service.result)
+
